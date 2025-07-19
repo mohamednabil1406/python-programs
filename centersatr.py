b6917285555007fbd1cef3631993500872efffdd
mohamednabil1406 <connectwithmhd@gmail.com>
2025-07-19 21:48:28 +0530

    new

---
+++ centersatr.py
@@ -0,0 +1,8 @@
+# pyramid pattern
+
+def pyramid_pattern(n):
+    for i in range(n, 0, -1):
+        spaces = '  ' * (n - i)
+        stars = '* ' * (2 * i - 1)
+        print(spaces + stars)
+pyramid_pattern(5)
---
+++ inversestart.py
@@ -0,0 +1,7 @@
+#inverse star pattern
+
+def star_inverse(n):
+    for i in range(n,0,-1):
+        print('* ' * i)
+
+star_inverse(5)

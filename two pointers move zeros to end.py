b9830b8287cf1f36b6725e97e880ff61ffbf2d20
mohamednabil1406 <connectwithmhd@gmail.com>
2025-07-22 22:12:42 +0530

    new pointers moves to zeros end

---
+++ two pointers move zeros to end.py
@@ -0,0 +1,10 @@
+def moves_zeroes(arr):
+    s = 0
+    for f in range(len(arr)):
+        if arr[f] != 0:
+            arr[s], arr[f] = arr[f], arr[s]
+            s += 1
+    return arr
+
+arr = [0, 1, 0, 2, 3, 5]
+print(moves_zeroes(arr))  # prints: [1, 2, 3, 5, 0, 0]
